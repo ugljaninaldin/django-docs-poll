08861fbd2c1d6ca98fff5ebd957ffce240446db0
Aldin Ugljanin <aldinugljaninn@gmail.com>
2023-11-11 19:14:39 +0100

    Django-Poll-App

---
+++ polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Question
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
